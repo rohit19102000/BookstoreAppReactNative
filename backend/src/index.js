49e680d8bb07a88c2df8e780066d8324e8287dc9
rohit kedar <rohitkedar2000@gmail.com>
2025-04-17 17:06:21 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import  'dotenv/config'
+
+import authRoutes from './routes/authRoutes.js'
+
+const app  = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT : ${PORT}`);
+})
